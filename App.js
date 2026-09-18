d05b2f4551df468c9961236e60ff9ce9a0c0e6b7
Okasha <okashasameer00@gmail.com>
2026-09-19 03:57:56 +0500

    Initial Laptop Store Expo app

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import './global.css';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-cyan-400">
+      <Text className="text-3xl font-bold text-black">
+        Welcome to Laptop Store
+      </Text>
+      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
